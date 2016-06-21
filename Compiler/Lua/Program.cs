211d8c09691afb28d708f2d0fbc870efb31f6b55
yanghuan <sy.yanghuan@gmail.com>
2016-06-21 12:38:12 +0800

    Simplify command-line arguments -l

--- Compiler/Lua/Program.cs
+++ Compiler/Lua/Program.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace Bridge.Lua {
     class Program {
-       private const string HelpCmdString = @"Usage: Bridge.Lua [-f srcfolder] [-p outfolder] [-lib thridlibs]
+       private const string HelpCmdString = @"Usage: Bridge.Lua [-f srcfolder] [-p outfolder] [-l thridlibs]
 Options and arguments 
 -f              : intput directory, all *.cs files whill be compiled
 -p              : out directory, will put the out lua files
--lib [option]   : third-party libraries referenced, use ';' to separate
--b [option]     : the path of bridge.all, defalut will use bridge.all under the same directory of Bridge.Lua
+-l [option]     : third-party libraries referenced, use ';' to separate
+-b [option]     : the path of bridge.all, defalut will use bridge.all under the same directory of Bridge.Lua.exe
 
 
 Compiled successfully, and then will have a manifest file to the output directory named manifest.lua, use require(""manifest.lua"")(you_put_dir) to load all
@@ -28,7 +28,7 @@ Compiled successfully, and then will have a manifest file to the output director
                     string folder = cmds.GetArgument("-f");
                     string output = cmds.GetArgument("-p");
                     string bridge = cmds.GetArgument("-b", true);
-                    string lib = cmds.GetArgument("-lib", true);
+                    string lib = cmds.GetArgument("-l", true);
                     Worker w = new Worker(folder, output, bridge, lib);
                     w.Do();
                     Console.WriteLine("all operator success");
