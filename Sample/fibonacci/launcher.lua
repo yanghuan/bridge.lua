deb37ee992ad168cd6682ec258f3ad6a4b9be452
yanghuan <sy.yanghuan@gmail.com>
2016-06-23 19:16:47 +0800

    remvoe '.'

--- Sample/fibonacci/launcher.lua
+++ Sample/fibonacci/launcher.lua
@@ -1,7 +1,7 @@
 package.path = package.path .. ";../__bin/coresystem.lua/?.lua"
 
 require("All")          -- coresystem.lua/All.lua
-require("out.manifest")("out.")    
+require("out.manifest")("out")    
 
 Test.Program.main()    -- run main method
 
