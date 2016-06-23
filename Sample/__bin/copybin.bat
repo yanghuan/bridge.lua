e304fe8c3d133e34c91cf84ebd60c6069ca666f8
yanghuan <sy.yanghuan@gmail.com>
2016-06-23 19:13:45 +0800

    change dir

---
+++ Sample/__bin/copybin.bat
@@ -0,0 +1,3 @@
+xcopy "..\..\Compiler\Lua\bin\Debug"  "bridge.lua"  /s /e /y 
+xcopy "..\..\Bridge\bin\Debug\Bridge.dll"  "bridge.lua" /y 
+xcopy "..\..\Compiler\Lua\CoreSystem.Lua"  "coresystem.lua"  /s /e /y 
--- Sample/copybin.bat
+++
@@ -1,3 +0,0 @@
-xcopy "..\Compiler\Lua\bin\Debug"  "__bin\bridge.lua"  /s /e /y 
-xcopy "..\Bridge\bin\Debug\Bridge.dll"  "__bin\bridge.lua" /y 
-xcopy "..\Compiler\Lua\CoreSystem.Lua"  "__bin\coresystem.lua"  /s /e /y 
